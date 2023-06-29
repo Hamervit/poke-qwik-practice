34219228f743d1de630a40c65f2a0f599008580a
Hamervit <haroldmercado44@gmail.com>
2023-06-28 23:01:05 -0500

    First signal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,30 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <span class="text-5xl">Hola mundo</span>;
+  const pokemonId = useSignal(1); // Primitive
+  // const pokemons = useStore()
+
+  return (
+    <>
+      <span class="text-2xl ">Buscardor simple</span>
+      <span class="text-9xl">{pokemonId} </span>
+      {/* TODO: Pokemon Image */}
+      <div class="mt-2">
+        <button class="btn btn-primary mr-2">Anterior</button>
+        <button class="btn btn-primary ">Siguiente</button>
+      </div>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik || Hello World",
+  title: "PokeQwik",
+
   meta: [
     {
       name: "description",
-      content: "Qwik site description for a new project",
+      content: "First qwik app",
     },
   ],
 };
